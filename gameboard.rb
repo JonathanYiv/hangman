1b4f82109066ad166965943e5d091b14a26e18f4
Jonathan Yiv <JonathanYiv@gmail.com>
2017-08-16 13:06:43 -0400

    Implemented Hangman functionality. Game is complete. Next commit will include save/load.

--- gameboard.rb
+++ gameboard.rb
@@ -1,12 +1,14 @@
 class GameBoard
-	attr_accessor :solution, :guesses
+	attr_accessor :solution, :correct_guesses, :incorrect_guesses
 
 	def initialize
 		@solution = random_solution
-		@guesses = Array.new(@solution.length, "_")
+		@correct_guesses = Array.new(@solution.length, "_")
+		@incorrect_guesses = []
 	end
 
 	def random_solution
+		word = File.open("5desk.txt").readlines.collect { |word| word.strip.downcase }.select { |word| word.length > 4 && word.length < 13 }.sample.split("")
 	end
 
 	def display
@@ -15,16 +17,78 @@ class GameBoard
 	end
 
 	def display_gallows
+		case @incorrect_guesses.length
+		when 0
+		puts "  ______"
+		puts "  |   \\|"
+		puts "       |"
+		puts "       |"
+		puts "       |"
+		puts "       |"
+		puts "-------^-------"
+		when 1
+		puts "  ______"
+		puts "  |   \\|"
+		puts "  O    |"
+		puts "       |"
+		puts "       |"
+		puts "       |"
+		puts "-------^-------"
+		when 2
+		puts "  ______"
+		puts "  |   \\|"
+		puts "  O    |"
+		puts "  |    |"
+		puts "       |"
+		puts "       |"
+		puts "-------^-------"
+		when 3
+		puts "  ______"
+		puts "  |   \\|"
+		puts "  O    |"
+		puts " -|    |"
+		puts "       |"
+		puts "       |"
+		puts "-------^-------"
+		when 4
+		puts "  ______"
+		puts "  |   \\|"
+		puts "  O    |"
+		puts " -|-   |"
+		puts "       |"
+		puts "       |"
+		puts "-------^-------"
+		when 5
+		puts "  ______"
+		puts "  |   \\|"
+		puts "  O    |"
+		puts " -|-   |"
+		puts "  ^    |"
+		puts "       |"
+		puts "-------^-------"
+		end
 	end
 
 	def display_guesses
+		puts "\nIncorrect Guesses: #{@incorrect_guesses.join(", ")}"
+		puts "Word: #{@correct_guesses.join(" ")}"
 	end
 
-	def update
-		evaluate_guess
+	def update(guess)
+		evaluate_guess(guess)
 		display
 	end
 
-	def evaluate_guess
+	def evaluate_guess(guess)
+		if @solution.include?(guess)
+			matching = @solution.each_index.select { |i| @solution[i] == guess }
+			matching.each { |i| @correct_guesses[i] = guess }
+		else
+			@incorrect_guesses << guess
+		end
+	end
+
+	def all_guesses
+		@correct_guesses + @incorrect_guesses
 	end
 end
--- hangman.rb
+++ hangman.rb
@@ -1,26 +1,94 @@
 require_relative 'gameboard.rb'
 
 class Hangman
-	attr_accessor :gameboard
+	attr_accessor :gameboard, :lose, :win
 
 	def initialize
 		@gameboard = GameBoard.new
+		@lose = false
+		@win = false
 	end
 
 	def play
+		welcome
 		@gameboard.display
 		instructions
 		#prompt_load
 		turns
 	end
 
+	def welcome
+		puts %{
+ _   _                                         
+| | | |                                        
+| |_| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+|  _  |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+\\_| |_/\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                    __/ |                      
+                   |___/                       
+		}
+	end
+
 	def instructions
+		puts "\nWelcome to Hangman!"
+		puts "\nThe computer will select a random 5-12 letter word."
+		puts "You can guess letters to figure the word out."
+		puts "You have 5 Guesses until the man is 'hung' and you lose!"
 	end
 
 	def turns
+		while @lose == false && @win == false
+			turn
+			check_status
+		end
+		lose if @lose
+		win if @win
+	end
+
+	def get_guess
+		guess = gets.chomp
+		until guess.downcase.match(/^[a-z]$/) && !@gameboard.all_guesses.include?(guess)
+			print "\nHmm.. something is wrong. Please type in a letter from A through Z that you haven't guessed yet.\n> "
+			guess = gets.chomp
+		end
+		guess
 	end
 
 	def turn
+		print "\nWhat is your guess?\n> "
+		@gameboard.update(get_guess)
+	end
+
+	def check_status
+		@lose = true if @gameboard.incorrect_guesses.length == 5
+		@win = true if !@gameboard.correct_guesses.include?("_")
+	end
+
+	def win
+		puts "\nCongratulations! You've guessed the word!"
+		again?
+	end
+
+	def lose
+		puts "\nThe answer was '#{@gameboard.solution.join("")}.'"
+		puts "Aww! You poor, poor thing. You've lost! Perhaps you ought to purchase a dictionary! Hahahaha!"
+		again?
+	end
+
+	def again?
+		print "\nWant to play again? Yes or No.\n> "
+		initialize
+		play if get_answer == "yes"
+	end
+
+	def get_answer
+		answer = gets.chomp.downcase
+		until answer == "yes" || answer == "no"
+			print "\nExcuse me. Did you say 'yes' or 'no?'\n> "
+			answer = gets.chomp.downcase
+		end
+		answer
 	end
 
 	def save
