0853421979e0594b74be436d0a44292800fb0a75
Jonathan Yiv <JonathanYiv@gmail.com>
2017-09-09 20:00:50 -0400

    Added visuals

--- app.rb
+++ app.rb
@@ -3,5 +3,15 @@ require 'sinatra/reloader' if development?
 require './lib/hangman.rb'
 
 get '/' do
-	erb :index, :locals => {:test => "test"}
+	erb :index
+end
+
+get '/start' do
+	incorrect = 0
+	erb :game, :locals => {:incorrect => incorrect + 1}
+end
+
+# a place to put reusable code snippets
+helpers do 
+
 end
